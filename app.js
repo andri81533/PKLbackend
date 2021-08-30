a8395b9f2912c6435689951bb8a18c04dd0b612a
idrisyoga23 <idrisyoga24@gmail.com>
2021-08-31 00:46:18 +0700

    cors policy not solve

--- app.js
+++ app.js
@@ -1,17 +1,37 @@
 var express = require('express')
 var bodyParser = require('body-parser')
 
+var dotenv =  require('dotenv')
+var cors = require('cors')
+dotenv.config()
+
 var app = express()
 
+
 app.use(
-    bodyParser.urlencoded({
-        extended: true
-    })
-)
+  bodyParser.urlencoded({
+    extended: true,
+  })
+);
+app.use(cors())
+app.use(bodyParser.json());
+// app.use(function (req, res, next) {
+//   res.header("Access-Control-Allow-Origin", "*");
+//   res.header(
+//     "Access-Control-Allow-Methods",
+//     "GET,HEAD,OPTIONS,POST,PUT,DELETE"
+//   );
+//   res.header(
+//     "Access-Control-Allow-Headers",
+//     "Origin, X-Requested-With, Content-Type, Accept, Authorization"
+//   );
+//   res.setHeader("content-type", "applciation/json");
+//   next();
+// });
 
-app.use(bodyParser.json())
+// app.use(express.static(path.join(__dirname, 'public')));
 
-var routes = require("./routes")
-routes(app)
+var routes = require("./routes");
+routes(app);
 
-module.exports = app
+module.exports = app;
--- database.js
+++ database.js
@@ -1,4 +1,5 @@
 const {Client} =  require("pg")
+var bcrypt = require('bcryptjs')
 
 const client = new Client({
     user: "postgres",
@@ -35,7 +36,8 @@ client.connect((err) => {
             var string = JSON.stringify(val)
             var users = JSON.parse(string)
             if(users.rowCount == 0){
-                let createUser = `INSERT INTO USERS(username, password, role) VALUES ('admin', 'admin', 'admin');`
+                var hashPassword = bcrypt.hashSync('admin123') 
+                let createUser = `INSERT INTO USERS(username, password, role) VALUES ('admin', '${hashPassword}', 'admin');`
                 client.query(createUser, function (error, results) {
                     if(error){
                         console.log(err)
@@ -48,9 +50,11 @@ client.connect((err) => {
         })
     })
     let createProjectTable = `CREATE TABLE IF NOT EXISTS PROJECT(id_project SERIAL NOT NULL PRIMARY KEY,
-        namaproject varchar(200) unique not null,
-        startdate varchar(200) not null,
-        enddate varchar(200) not null
+        nama_project varchar(200) unique not null,
+        start_date varchar(200) not null,
+        end_date varchar(200) not null,
+        nama_pm varchar(200) not null,
+        task_list varchar(200) not null
         );`;
     
     client.query(createProjectTable, (err, results) => {
@@ -70,7 +74,7 @@ client.connect((err) => {
             var string = JSON.stringify(val)
             var project = JSON.parse(string)
             if(project.rowCount == 0){
-                let createproject = `INSERT INTO PROJECT(namaproject, startdate, enddate) VALUES ('Test', '2020-2-1', '2020-2-13');`
+                let createproject = `INSERT INTO PROJECT(nama_project, start_date, end_date, nama_pm, task_list) VALUES ('Test', '2020-2-1', '2020-2-13', 'undified', 'undified');`
                 client.query(createproject, function (error, results) {
                     if(error){
                         console.log(err)
@@ -83,11 +87,14 @@ client.connect((err) => {
         })
     })
     let createTaskTable = `CREATE TABLE IF NOT EXISTS TASK(id_task SERIAL NOT NULL PRIMARY KEY,
-        namatask varchar(200) unique not null,
-        startdate varchar(200) not null,
-        enddate varchar(200) not null,
+        nama_task varchar(200) unique not null,
+        start_date varchar(200) not null,
+        end_date varchar(200) not null,
+        nama_karyawan varchar(200) not null,
         status varchar(200) not null,
-        deskripsi varchar(200) not null
+        deskripsi varchar(200) not null,
+        file varchar(200) not null
+        
         );`;
     
     client.query(createTaskTable, (err, results) => {
@@ -107,7 +114,7 @@ client.connect((err) => {
             var string = JSON.stringify(val)
             var task = JSON.parse(string)
             if(task.rowCount == 0){
-                let createTask = `INSERT INTO TASK(namatask, startdate, enddate, status, deskripsi) VALUES ('Test', '2020-2-1', '2020-2-13', 'diterima', 'test');`
+                let createTask = `INSERT INTO TASK(nama_task, start_date, end_date, nama_karyawan, status, deskripsi, file) VALUES ('Test', '2020-2-1', '2020-2-13', 'Agung', 'diterima', 'test', 'test');`
                 client.query(createTask, function (error, results) {
                     if(error){
                         console.log(err)
@@ -121,7 +128,8 @@ client.connect((err) => {
     })
     let createKomenTable = `CREATE TABLE IF NOT EXISTS KOMEN(id_KOMEN SERIAL NOT NULL PRIMARY KEY,
         upload TIMESTAMP,
-        teks varchar(1000) not null
+        teks varchar(1000) not null,
+        nama_pm varchar(200) not null
         );`;
     
     client.query(createKomenTable, (err, results) => {
@@ -141,7 +149,7 @@ client.connect((err) => {
             var string = JSON.stringify(val)
             var komen = JSON.parse(string)
             if(komen.rowCount == 0){
-                let createKomen = `INSERT INTO KOMEN(upload, teks) VALUES ('2008-01-01 00:00:01', 'KURANGBRO');`
+                let createKomen = `INSERT INTO KOMEN(upload, teks, nama_pm) VALUES ('2008-01-01 00:00:01', 'KURANGBRO', 'test');`
                 client.query(createKomen, function (error, results) {
                     if(error){
                         console.log(err)
